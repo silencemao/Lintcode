83d7818e69126991b1c520f05e5569ded55d7a23
SF <sf@MacBook-Pro-2.local>
2019-08-11 12:11:38 +0800

    幂次方

--- Leetcode_50_myPow.java
+++ Leetcode_50_myPow.java
@@ -1,12 +1,14 @@
 package main.java;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
 /**
  * 幂次方
  * */
 public class Leetcode_50_myPow {
     public static void main(String[] args){
         double x = 2;
-        int n = -10;
+        int n = Integer.MIN_VALUE;
         System.out.println(myPow(x, n));
         System.out.println(myPow1(x, n));
         System.out.println(myPow2(x, n));
@@ -14,6 +16,7 @@ public class Leetcode_50_myPow {
 
     public static double myPow(double x, int n){
         if (n==0) return 1;
+        if (n==Integer.MIN_VALUE && x > 1) return 0.0;
         if (n < 0){
             x = 1/x;
             n = -n;
@@ -23,6 +26,7 @@ public class Leetcode_50_myPow {
 
     public static double myPow1(double x, int n){
         if (n==0) return 1;
+        if (n==Integer.MIN_VALUE && x > 1) return 0.0;
         if (n < 0){
             x = 1/x;
             n = -n;
@@ -38,12 +42,12 @@ public class Leetcode_50_myPow {
     /**
      * N=9 二进制 1001
      * x^9 = x^(1001) = x^(1*2^3 + 0*2^2 + 0*2^1 + 1*2*0) = x^(8+1) = x^8 * x^1
-     * 
+     *
      * */
     public static double myPow2(double x, int n){
         if (n==0) return 1;
         double res = 1;
-        long absN = Math.abs(n);
+        long absN = Math.abs((long)n);
         while (absN > 0){
             if ((absN & 1)==1)
                 res *= x;
