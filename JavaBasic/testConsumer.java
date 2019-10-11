c0c7bf49784ed7994d559ed0dea315c3d65f88fe
silencemao <17862953895@163.com>
2019-10-11 11:02:14 +0800

    add map filter flatmap max min

--- JavaBasic/testConsumer.java
+++ JavaBasic/testConsumer.java
@@ -1,13 +1,19 @@
 package main.java.JavaBasic;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class testConsumer {
     public static void main(String[] args) {
         testAccept();
         testAddThen();
+        testMap();
+        testFilter();
+        testFlatMap();
+        testmaxMin();
     }
 
     /**
@@ -67,5 +73,48 @@ public class testConsumer {
 
         // using addThen()
         modify.andThen(dispList).accept(list);
+        System.out.println();
+    }
+
+    /**
+     * map实现对集合中的所有数据进行统一的操作，代替for循环挨个遍历
+     * */
+    public static void testMap() {
+        List<String> collected = Stream.of("a", "b", "hello").map(String::toUpperCase).collect(Collectors.toList());
+        collected.forEach(System.out::println);
+    }
+    /**
+     * filter 对集合中的数据进行过滤，将结果存放至新的list中，对原始数据没有影响
+     *
+     * */
+    public static void testFilter() {
+        List<String> collected = new ArrayList<>();
+        collected.add("10"); collected.add("1abc"); collected.add("mnq"); collected.add("10h");
+        List<String> result = collected.stream().filter(string -> Character.isDigit(string.charAt(0))).collect(Collectors.toList());
+//        System.out.println(result.toString());
+        result.forEach(System.out::println);
+    }
+    /**
+     * flatMap 合并集合
+     * */
+    public static void testFlatMap() {
+//        List<Integer> together = Stream.of()
+        Integer[][] collected = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        Stream<Integer> result = Arrays.stream(collected).flatMap(x -> Arrays.stream(x));
+        result.forEach(x -> System.out.println(x));
+    }
+
+    public static void testmaxMin() {
+        System.out.println("test max min");
+//        List<Integer> collected = Arrays.asList(1, 20, 3, 9, 30);  // 这种初始化方法,collected的size固定了,不能再add,remove了
+//        collected.remove(collected.size()-1);
+
+        List<Integer> collected = new ArrayList<>(Arrays.asList(1, 20, 3, 9, 30));  // 这个list的size不固定，可以增加/删除
+        collected.add(50);
+        collected.forEach(x -> System.out.println(x));
+        int maxInt = Collections.max(collected);
+        int minInt = Collections.min(collected);
+        System.out.println(maxInt);
+        System.out.println(minInt);
     }
 }
